ac4cfd2baa6e55b86f3d0260d81ef9c24c0301a8
Maksim Mironov <terrran@gmail.com>
2017-01-14 21:12:21 +0300

    тест для функции

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ $user = new \models\User();
 $person = new \models\Person();
 
 
+$user->email = 'test@test.ru';
+$user->name = 'Уася';
+
 function sendMail($name, $email) {
     echo 'почта для ' . $name . ' отправлена на адрес ' . $email;
 }
+
+sendMail($user->name, $user->email);

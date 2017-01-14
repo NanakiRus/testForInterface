ec73e3ce9d04fd5f3015c2a2a330391b24a0f3ce
Maksim Mironov <terrran@gmail.com>
2017-01-14 21:10:05 +0300

    add function

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
 require __DIR__ . '/autoload.php';
 
 $client = new \models\Client();
+$user = new \models\User();
+$person = new \models\Person();
+
+
+function sendMail($name, $email) {
+    echo 'почта для ' . $name . ' отправлена на адрес ' . $email;
+}
